7e7c9a36a61e708a27f3e6fa193e8c5f5fd7747c
MuhdHanish <muhammedhanish11@gmail.com>
2025-02-19 20:45:57 +0530

    feat: creatd actoin button component

---
+++ src/renderer/src/components/buttons/action-button.tsx
@@ -0,0 +1,14 @@
+import { ButtonProps } from '@renderer/types'
+import { twMerge } from 'tailwind-merge'
+
+export const ActionButton = ({ children, className, ...props }: ButtonProps) => {
+  return (
+    <button
+      {...props}
+      className={twMerge(
+        'px-2 py-1 rounded-md border border-zinc-400/50 hover:bg-zinc-600/50 transition-colors duration-100',
+        className
+      )}
+    ></button>
+  )
+}
