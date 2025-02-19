c8aa3dcf11bde01589ec1ef6c0d16a0ee3233803
MuhdHanish <muhammedhanish11@gmail.com>
2025-02-19 09:15:02 +0530

    chore: updated electron.vite.config

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -4,15 +4,30 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin()],
+    resolve: {
+      alias: {
+        '@lib': resolve('src/main/lib'),
+        '@shared': resolve('src/shared')
+      }
+    }
   },
   preload: {
     plugins: [externalizeDepsPlugin()]
   },
   renderer: {
+    assetsInclude: ['src/renderer/src/assets/**'],
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
+        '@renderer': resolve('src/renderer/src'),
+        '@shared': resolve('src/shared'),
+        '@hooks': resolve('src/renderer/src/hooks'),
+        '@utils': resolve('src/renderer/src/utils'),
+        '@assets': resolve('src/renderer/src/assets'),
+        '@store': resolve('src/renderer/src/store'),
+        '@components': resolve('src/renderer/src/components'),
+        '@types': resolve('src/renderer/src/types'),
+        '@mocks': resolve('src/renderer/src/mocks')
       }
     },
     plugins: [react()]
