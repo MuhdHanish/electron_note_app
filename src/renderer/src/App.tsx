44fb87b89b979d4a90b3c796943f693e5f9051a4
MuhdHanish <muhammedhanish11@gmail.com>
2025-02-19 11:11:30 +0530

    refactor: just added a basic code

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,7 @@
 function App(): JSX.Element {
-  return <div></div>
+  return <div className="h-full flex-center">
+    <span className="text-xl text-teal-500 font-bold">Electron Note App</span>
+  </div>
 }
 
 export default App
