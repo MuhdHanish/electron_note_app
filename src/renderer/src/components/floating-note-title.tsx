e0fbff4056d36069a49e812cfb92492e66afc15d
MuhdHanish <muhammedhanish11@gmail.com>
2025-02-20 12:44:30 +0530

    refactor: formatted file

--- src/renderer/src/components/floating-note-title.tsx
+++ src/renderer/src/components/floating-note-title.tsx
@@ -6,7 +6,7 @@ import { twMerge } from 'tailwind-merge'
 export const FloatingNoteTitle = ({ className, ...props }: ComponentProps<'div'>) => {
   const selectedNote = useAtomValue(selectedNoteAtom)
   if (!selectedNote) return null
-  
+
   return (
     <div className={twMerge('flex justify-center', className)} {...props}>
       <span className="text-gray-400">{selectedNote?.title || 'Untitled'}</span>
