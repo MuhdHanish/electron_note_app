d8d014ac6f0c7a5d3cdec7d3cd0b3ac4de08581e
MuhdHanish <muhammedhanish11@gmail.com>
2025-02-20 10:31:11 +0530

    refactor: formatted files

--- src/renderer/src/components/note-preview.tsx
+++ src/renderer/src/components/note-preview.tsx
@@ -28,7 +28,9 @@ export const NotePreview = ({
       )}
     >
       <h3 className="mb-1 font-bold truncate">{title}</h3>
-      <span className="inline-block w-full mb-2 text-xs font-light text-left text-zinc-400">{formattedDate}</span>
+      <span className="inline-block w-full mb-2 text-xs font-light text-left text-zinc-400">
+        {formattedDate}
+      </span>
     </div>
   )
 }
