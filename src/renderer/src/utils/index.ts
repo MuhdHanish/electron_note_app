3b58105fea1aabc3ae99f73c17ff119d45a27049
MuhdHanish <muhammedhanish11@gmail.com>
2025-02-20 09:00:39 +0530

    feat: added date formatter util function

--- src/renderer/src/utils/index.ts
+++ src/renderer/src/utils/index.ts
@@ -4,3 +4,11 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+const dateFormatter = new Intl.DateTimeFormat('en-US', {
+  dateStyle: 'short',
+  timeStyle: 'short',
+  timeZone: 'UTC'
+})
+
+export const formatDateFromMS = (ms: number) => dateFormatter.format(ms)
