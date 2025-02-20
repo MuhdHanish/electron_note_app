f2ceda0ea4e5b318ddaf5a873417ed240f37aac2
MuhdHanish <muhammedhanish11@gmail.com>
2025-02-20 09:46:37 +0530

    feat: added checking notes there or not

--- src/renderer/src/components/note-preview-list.tsx
+++ src/renderer/src/components/note-preview-list.tsx
@@ -1,11 +1,21 @@
 import { NOTES_MOCK } from '@/store/mocks'
 import { ComponentProps } from 'react'
 import { NotePreview } from './note-preview'
+import { twMerge } from 'tailwind-merge'
 
-export const NotePreviewList = ({ ...props }: ComponentProps<'ul'>) => {
+export const NotePreviewList = ({ className, ...props }: ComponentProps<'ul'>) => {
+  const notes = NOTES_MOCK
+  if (!notes?.length) {
+    return (
+      <ul {...props} className={twMerge('text-center pt-4', className)}>
+        <span>No Notes Yet!</span>
+      </ul>
+    )
+  }
+
   return (
     <ul {...props}>
-      {NOTES_MOCK?.map((note, index) => (
+      {notes?.map((note, index) => (
         <NotePreview {...note} key={`${note?.title}-${index}-${note?.updatedAt}`} />
       ))}
     </ul>
