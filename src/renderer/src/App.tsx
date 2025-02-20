0f902d79adc1e75ac3f9601c5dee75967023fde4
MuhdHanish <muhammedhanish11@gmail.com>
2025-02-20 08:15:55 +0530

    feat: added action-buttons-row component in app

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,11 +1,13 @@
-import { Sidebar, Content, RootLayout, DraggableTopBar } from '@/components'
+import { Sidebar, Content, RootLayout, DraggableTopBar, ActionButtonsRow } from '@/components'
 
 function App(): JSX.Element {
   return (
     <>
       <DraggableTopBar />
       <RootLayout>
-        <Sidebar className="p-2"></Sidebar>
+        <Sidebar className="p-2">
+          <ActionButtonsRow className='flex justify-between mt-1'/>
+        </Sidebar>
         <Content className="border-l border-l-white/20 bg-zinc-900/50"></Content>
       </RootLayout>
     </>
