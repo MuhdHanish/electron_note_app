c1e7ad3981c9bca0af21b625729fbf6e61576dad
MuhdHanish <muhammedhanish11@gmail.com>
2025-02-20 10:31:28 +0530

    feat: created a markdown editro compoent using mdxeditor

---
+++ src/renderer/src/components/markdown-editor.tsx
@@ -0,0 +1,18 @@
+import {
+  headingsPlugin,
+  listsPlugin,
+  markdownShortcutPlugin,
+  MDXEditor,
+  quotePlugin
+} from '@mdxeditor/editor'
+
+export const MarkdownEditor = () => {
+  return (
+    <MDXEditor
+      markdown={`## Welcome to Electron Note App 👋  
+<span style="color:gray;">Start writing your notes here...</span>`}
+      plugins={[quotePlugin(), listsPlugin(), headingsPlugin(), markdownShortcutPlugin()]}
+      contentEditableClassName="outline-none min-h-screen overflow-auto max-w-none text-lg px-8 py-5 caret-yellow-500 prose prose-invert prose-p:my-3 prose-p:leading-relaxed prose-headings:my-4 prose-blockquote:my-4 prose-ul:my-2 prose-li:my-0 prose-code:px-1 prose-code:text-red-500 prose-code:before:content-[''] prose-code:after:content-['']"
+    />
+  )
+}
