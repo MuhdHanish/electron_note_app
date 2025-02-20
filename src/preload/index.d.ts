a640e20689fab764170ed2c5fb6ab625615391c3
MuhdHanish <muhammedhanish11@gmail.com>
2025-02-20 09:40:37 +0530

    feat: added locale date time option in formatter

--- src/preload/index.d.ts
+++ src/preload/index.d.ts
@@ -1,8 +1,12 @@
 // import { ElectronAPI } from '@electron-toolkit/preload'
 
+export { };
+  
 declare global {
   interface Window {
     // electron: ElectronAPI
-    context: {}
+    context: {
+      locale: string
+    }
   }
 }
--- src/renderer/src/utils/index.ts
+++ src/renderer/src/utils/index.ts
@@ -5,10 +5,11 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-const dateFormatter = new Intl.DateTimeFormat('en-US', {
+const dateFormatter = new Intl.DateTimeFormat(window.context.locale, {
   dateStyle: 'short',
   timeStyle: 'short',
-  timeZone: 'UTC'
+  timeZone: 'UTC',
+  hour12: true
 })
 
-export const formatDateFromMS = (ms: number) => dateFormatter.format(ms)
+export const formatDateFromMS = (ms: number) => dateFormatter.format(ms)?.replace(/\b(am|pm)\b/g, (match) => match?.toUpperCase());
