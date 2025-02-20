a9be2bfa1e3054a4829d2d1bf0e53a0a39572eb5
MuhdHanish <muhammedhanish11@gmail.com>
2025-02-20 21:52:03 +0530

    feat: added actions in create and delete note button

--- src/renderer/src/components/buttons/delete-note-button.tsx
+++ src/renderer/src/components/buttons/delete-note-button.tsx
@@ -1,10 +1,16 @@
 import { ActionButton } from '@/components'
+import { deleteNoteAtom } from '@renderer/store'
 import { ButtonProps } from '@shared/models'
+import { useSetAtom } from 'jotai'
 import { Trash } from 'lucide-react'
 
 export const DeleteNoteButton = ({ ...props }: ButtonProps) => {
+  const deleteNote = useSetAtom(deleteNoteAtom)
+
+  const handleDeletion = () => deleteNote()
+
   return (
-    <ActionButton {...props}>
+    <ActionButton onClick={handleDeletion} {...props}>
       <Trash className="size-3.5 text-zinc-300" />
     </ActionButton>
   )
--- src/renderer/src/components/buttons/new-note-button.tsx
+++ src/renderer/src/components/buttons/new-note-button.tsx
@@ -1,10 +1,16 @@
 import { ActionButton } from '@/components'
+import { createEmptyNoteAtom } from '@renderer/store'
 import { ButtonProps } from '@shared/models'
+import { useSetAtom } from 'jotai'
 import { FilePen } from 'lucide-react'
 
 export const NewNoteButton = ({ ...props }: ButtonProps) => {
+  const createEmptyNote = useSetAtom(createEmptyNoteAtom)
+
+  const handleCreation = () => createEmptyNote()
+
   return (
-    <ActionButton {...props}>
+    <ActionButton onClick={handleCreation} {...props}>
       <FilePen className="size-3.5 text-zinc-300" />
     </ActionButton>
   )
