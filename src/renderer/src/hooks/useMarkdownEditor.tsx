b4f74645d3757d4de0278c5f7a66c2491e2076e7
MuhdHanish <muhammedhanish11@gmail.com>
2025-02-21 21:17:05 +0530

    feat: added saveNote & editorRef in useMarkdownEditor hook

--- src/renderer/src/hooks/useMarkdownEditor.tsx
+++ src/renderer/src/hooks/useMarkdownEditor.tsx
@@ -1,9 +1,16 @@
-import { selectedNoteAtom } from '@renderer/store'
-import { useAtomValue } from 'jotai'
+import { MDXEditorMethods } from '@mdxeditor/editor'
+import { saveNoteAtom, selectedNoteAtom } from '@renderer/store'
+import { useAtomValue, useSetAtom } from 'jotai'
+import { useRef } from 'react'
 
 export const useMarkdownEditor = () => {
   const selectedNote = useAtomValue(selectedNoteAtom)
+  const saveNote = useSetAtom(saveNoteAtom)
+  const editorRef = useRef<MDXEditorMethods | null>(null)
+
   return {
-    selectedNote
+    selectedNote,
+    saveNote,
+    editorRef
   }
 }
