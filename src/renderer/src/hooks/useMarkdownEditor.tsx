4a3aeef022783025f9df54ef041cd6c3d4318d35
MuhdHanish <muhammedhanish11@gmail.com>
2025-02-21 21:24:28 +0530

    feat: used SAVING_INTERVAL in handleSaving

--- src/renderer/src/hooks/useMarkdownEditor.tsx
+++ src/renderer/src/hooks/useMarkdownEditor.tsx
@@ -1,5 +1,6 @@
 import { MDXEditorMethods } from '@mdxeditor/editor'
 import { saveNoteAtom, selectedNoteAtom } from '@renderer/store'
+import { SAVING_INTERVAL } from '@shared/constance'
 import { useAtomValue, useSetAtom } from 'jotai'
 import { throttle } from 'lodash'
 import { useRef } from 'react'
@@ -9,16 +10,20 @@ export const useMarkdownEditor = () => {
   const saveNote = useSetAtom(saveNoteAtom)
   const editorRef = useRef<MDXEditorMethods | null>(null)
 
-  const handleAutoSaving = throttle(async () => { 
-    if (!selectedNote) return
-    
-    console.info(`Auto saving: ${selectedNote?.title}`)
+  const handleAutoSaving = throttle(
+    async () => {
+      if (!selectedNote) return
 
-    await saveNote(selectedNote?.content)
-  }, 3000, {
-    leading: false,
-    trailing: true
-  })
+      console.info(`Auto saving: ${selectedNote?.title}`)
+
+      await saveNote(selectedNote?.content)
+    },
+    SAVING_INTERVAL,
+    {
+      leading: false,
+      trailing: true
+    }
+  )
 
   return {
     selectedNote,
