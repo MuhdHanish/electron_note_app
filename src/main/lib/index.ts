849cb410bf2c35a457598c2415c3bb4963a5d27e
MuhdHanish <muhammedhanish11@gmail.com>
2025-02-21 21:43:44 +0530

    refactor: changed loging msg

--- src/main/lib/index.ts
+++ src/main/lib/index.ts
@@ -41,7 +41,7 @@ export const readNote: ReadNote = async (fileName) => {
 export const writeNote: WriteNote = async (fileName, content) => {
   const rootDir = getRootDir()
 
-  console.info(`Writing note to ${rootDir}/${fileName}.md`)
+  console.info(`Writing note to ${fileName}`)
 
   return writeFile(`${rootDir}/${fileName}.md`, content, FILE_ENCODING)
 }
