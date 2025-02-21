a167970f281bdc29000cdb6cb4f41f9750af396c
MuhdHanish <muhammedhanish11@gmail.com>
2025-02-21 09:55:38 +0530

    feat: added main/lib file conatin the interactive layer between the file system and app

---
+++ src/main/lib/index.ts
@@ -0,0 +1,33 @@
+import { APP_DIRECTORY_NAME, FILE_ENCODING } from '@shared/constance'
+import { NoteInfo } from '@shared/models'
+import { GetNotes } from '@shared/types'
+import { ensureDir, readdir, stat } from 'fs-extra'
+import { homedir } from 'os'
+
+export const getRootDir = () => {
+  return `${homedir}/${APP_DIRECTORY_NAME}`
+}
+
+export const getNotes: GetNotes = async () => {
+  const rootDir = getRootDir()
+
+  await ensureDir(rootDir)
+
+  const notesFileNames = await readdir(rootDir, {
+    encoding: FILE_ENCODING,
+    withFileTypes: false
+  })
+
+  const notes = notesFileNames?.filter((fileName) => fileName?.endsWith('.md'))
+
+  return Promise.all(notes?.map(getNoteInfoFromFileName))
+}
+
+export const getNoteInfoFromFileName = async (fileName: string): Promise<NoteInfo> => {
+  const fileStats = await stat(`${getRootDir()}/${fileName}`)
+
+  return {
+    title: fileName.replace(/\.md$/, ''),
+    updatedAt: fileStats.mtimeMs
+  }
+}
