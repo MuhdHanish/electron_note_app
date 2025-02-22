4263165786d03de3161bc318a82b89d2c8ab7952
MuhdHanish <muhammedhanish11@gmail.com>
2025-02-22 07:18:35 +0530

    feat: added create note function in ipcMain.handle

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,8 +2,8 @@ import { app, shell, BrowserWindow, ipcMain } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
-import { getNotes, readNote, writeNote } from './lib'
-import { GetNotes, ReadNote, WriteNote } from '@shared/types'
+import { createNote, getNotes, readNote, writeNote } from './lib'
+import { CreateNote, GetNotes, ReadNote, WriteNote } from '@shared/types'
 
 function createWindow(): void {
   // Create the browser window.
@@ -63,6 +63,7 @@ app.whenReady().then(() => {
   ipcMain.handle('getNotes', async (_, ...args: Parameters<GetNotes>) => getNotes(...args))
   ipcMain.handle('readNote', async (_, ...args: Parameters<ReadNote>) => readNote(...args))
   ipcMain.handle('writeNote', async (_, ...args: Parameters<WriteNote>) => writeNote(...args))
+  ipcMain.handle('createNote', async (_, ...args: Parameters<CreateNote>) => createNote(...args))
 
   createWindow()
 
