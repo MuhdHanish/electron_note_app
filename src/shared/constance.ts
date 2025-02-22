8925eae9adb67459ddc54734a3d2f4a9e389cc4c
MuhdHanish <muhammedhanish11@gmail.com>
2025-02-22 07:59:17 +0530

    chore: added welcome note file name in constance

--- src/shared/constance.ts
+++ src/shared/constance.ts
@@ -2,3 +2,5 @@ export const APP_DIRECTORY_NAME = 'electron_note_app_notes'
 export const FILE_ENCODING = 'utf-8'
 
 export const SAVING_INTERVAL = 3000
+
+export const WELCOME_NOTE_FILE_NAME = 'Welcome.md'
